47cc97b16c011ad271e372f180e3bc05cb80c4d0
mcada <mcada@ADAM>
2023-09-26 16:20:06 +0200

    Aggiungere i file di progetto.

---
+++ Tetris/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Tetris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxWb = new System.Windows.Forms.PictureBox();
+            this.pictureBoxWr = new System.Windows.Forms.PictureBox();
+            this.pictureBoxWl = new System.Windows.Forms.PictureBox();
+            this.pictureBoxWt = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWr)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBoxWb
+            // 
+            this.pictureBoxWb.BackColor = System.Drawing.Color.Gray;
+            this.pictureBoxWb.Location = new System.Drawing.Point(175, 660);
+            this.pictureBoxWb.Name = "pictureBoxWb";
+            this.pictureBoxWb.Size = new System.Drawing.Size(300, 10);
+            this.pictureBoxWb.TabIndex = 0;
+            this.pictureBoxWb.TabStop = false;
+            // 
+            // pictureBoxWr
+            // 
+            this.pictureBoxWr.BackColor = System.Drawing.Color.Gray;
+            this.pictureBoxWr.Location = new System.Drawing.Point(475, 50);
+            this.pictureBoxWr.Name = "pictureBoxWr";
+            this.pictureBoxWr.Size = new System.Drawing.Size(10, 620);
+            this.pictureBoxWr.TabIndex = 1;
+            this.pictureBoxWr.TabStop = false;
+            // 
+            // pictureBoxWl
+            // 
+            this.pictureBoxWl.BackColor = System.Drawing.Color.Gray;
+            this.pictureBoxWl.Location = new System.Drawing.Point(165, 50);
+            this.pictureBoxWl.Name = "pictureBoxWl";
+            this.pictureBoxWl.Size = new System.Drawing.Size(10, 620);
+            this.pictureBoxWl.TabIndex = 2;
+            this.pictureBoxWl.TabStop = false;
+            // 
+            // pictureBoxWt
+            // 
+            this.pictureBoxWt.BackColor = System.Drawing.Color.Gray;
+            this.pictureBoxWt.Location = new System.Drawing.Point(175, 50);
+            this.pictureBoxWt.Name = "pictureBoxWt";
+            this.pictureBoxWt.Size = new System.Drawing.Size(300, 10);
+            this.pictureBoxWt.TabIndex = 3;
+            this.pictureBoxWt.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(33, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Next Block";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(506, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Score: ";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(506, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Lines: ";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(506, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(506, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(665, 730);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxWt);
+            this.Controls.Add(this.pictureBoxWl);
+            this.Controls.Add(this.pictureBoxWr);
+            this.Controls.Add(this.pictureBoxWb);
+            this.Name = "Form1";
+            this.Text = "Tetris";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWr)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWt)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBoxWb;
+        private System.Windows.Forms.PictureBox pictureBoxWr;
+        private System.Windows.Forms.PictureBox pictureBoxWl;
+        private System.Windows.Forms.PictureBox pictureBoxWt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Tetris/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * 2 schermate:
+ *  impostazioni per cambiare i pulsanti, volume, colori(forse);
+ *  difficolta;
+ */
+namespace Tetris
+{
+    public partial class Form1 : Form
+    {
+        public PictureBox[,] pictureBox = new PictureBox[20, 10];
+        public PictureBox[,] pictureBoxNB = new PictureBox[4, 3];
+        public Random Block = new Random(); //7 blocks
+        public int Bl;
+        public int NextBlock;
+
+        public int VelocityY = 1;
+        public int PosXL;
+        public int PosXR;
+        public int X=0;
+        public int OldY = 0;
+        public int OldX = 0;
+        public int PosY;
+        public bool Empty = false;
+
+        public bool KeyDown_Down = false;
+
+        public int Score;
+        public int Lines;
+        public Form1() //30 pixel = 1;
+        {
+            InitializeComponent();
+        }
+
+        #region Movement
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Q || e.KeyCode == Keys.LButton)
+            {
+            }
+            if (e.KeyCode == Keys.E || e.KeyCode == Keys.RButton)
+            {
+            }
+            //
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                if (PosXL!= 0)
+                {
+                    X--;
+                    PosXL--;
+                    PosXR--;
+                }
+            }
+            if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                if(PosXR!= 9)
+                {
+                    X++;
+                    PosXL++;
+                    PosXR++;
+                }
+            }
+            //
+            if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                timer1.Interval = 33;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            timer1.Interval = 1000;
+        }
+        #endregion
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!Empty)
+            {
+                X = 0; OldX = 0; OldY = 0;
+                Bl = Block.Next(1, 8);
+                switch (Bl)
+                {
+                    case 1:
+                        PosXL = 5; PosXR = 5;
+                        PosY = 3;
+                        break;
+                    case 2:
+                        PosXL = 4; PosXR = 6;
+                        PosY = 1;
+                        break;
+                    case 3:
+                        PosXL = 4; PosXR = 6;
+                        PosY = 1;
+                        break;
+                    case 4:
+                        PosXL = 4; PosXR = 5;
+                        PosY = 1;
+                        break;
+                    case 5:
+                        PosXL = 4; PosXR = 6;
+                        PosY = 1;
+                        break;
+                    case 6:
+                        PosXL = 4; PosXR = 6;
+                        PosY = 1;
+                        break;
+                    case 7:
+                        PosXL = 4; PosXR = 6;
+                        PosY = 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (PosY == 19)
+            {
+                Empty = true;
+            }
+            else if (PosY >= 20)
+            {
+                Empty = false;
+            }
+            else if (pictureBox[PosY + 1, PosXL].BackColor != Color.Transparent || pictureBox[PosY + 1, PosXL + 1].BackColor != Color.Transparent || pictureBox[PosY + 1, PosXR].BackColor != Color.Transparent) 
+            {
+                Empty = true;
+            }
+            else
+            {
+                Empty = false;
+            }
+
+            if (Empty)
+            {
+                if (4 + OldX != -1)
+                {
+                    pictureBox[OldY, 4 + OldX].BackColor = Color.Transparent;
+                    pictureBox[1 + OldY, 4 + OldX].BackColor = Color.Transparent;
+                }    
+                pictureBox[OldY, 5 + OldX].BackColor = Color.Transparent;
+                if(6 + OldX != 10)
+                {
+                    pictureBox[OldY, 6 + OldX].BackColor = Color.Transparent;
+                    pictureBox[1 + OldY, 6 + OldX].BackColor = Color.Transparent;
+                }
+                pictureBox[1 + OldY, 5 + OldX].BackColor = Color.Transparent;
+                if(3 + OldY != 20)
+                {
+                    pictureBox[2 + OldY, 5 + OldX].BackColor = Color.Transparent;
+                    pictureBox[3 + OldY, 5 + OldX].BackColor = Color.Transparent;
+                }
+                OldX = X;
+
+                switch (Bl)
+                {
+                    case 1:
+                        pictureBox[0 + PosY - 3, 5 + X].BackColor = Color.LightBlue;
+                        pictureBox[1 + PosY - 3, 5 + X].BackColor = Color.LightBlue;
+                        pictureBox[2 + PosY - 3, 5 + X].BackColor = Color.LightBlue;
+                        pictureBox[3 + PosY - 3, 5 + X].BackColor = Color.LightBlue;
+                        OldY = PosY - 3;
+                        break;
+                    case 2:
+                        pictureBox[0 + PosY - 1, 4 + X].BackColor = Color.Blue;
+                        pictureBox[1 + PosY - 1, 4 + X].BackColor = Color.Blue;
+                        pictureBox[1 + PosY - 1, 5 + X].BackColor = Color.Blue;
+                        pictureBox[1 + PosY - 1, 6 + X].BackColor = Color.Blue;
+                        OldY = PosY - 1;
+                        break;
+                    case 3:
+                        pictureBox[0 + PosY - 1, 6 + X].BackColor = Color.Orange;
+                        pictureBox[1 + PosY - 1, 4 + X].BackColor = Color.Orange;
+                        pictureBox[1 + PosY - 1, 5 + X].BackColor = Color.Orange;
+                        pictureBox[1 + PosY - 1, 6 + X].BackColor = Color.Orange;
+                        OldY = PosY - 1;
+                        break;
+                    case 4:
+                        pictureBox[0 + PosY - 1, 4 + X].BackColor = Color.Yellow;
+                        pictureBox[0 + PosY - 1, 5 + X].BackColor = Color.Yellow;
+                        pictureBox[1 + PosY - 1, 4 + X].BackColor = Color.Yellow;
+                        pictureBox[1 + PosY - 1, 5 + X].BackColor = Color.Yellow;
+                        OldY = PosY - 1;
+                        break;
+                    case 5:
+                        pictureBox[0 + PosY - 1, 5 + X].BackColor = Color.Green;
+                        pictureBox[0 + PosY - 1, 6 + X].BackColor = Color.Green;
+                        pictureBox[1 + PosY - 1, 4 + X].BackColor = Color.Green;
+                        pictureBox[1 + PosY - 1, 5 + X].BackColor = Color.Green;
+                        OldY = PosY - 1;
+                        break;
+                    case 6:
+                        pictureBox[0 + PosY - 1, 4 + X].BackColor = Color.Purple;
+                        pictureBox[0 + PosY - 1, 5 + X].BackColor = Color.Purple;
+                        pictureBox[1 + PosY - 1, 5 + X].BackColor = Color.Purple;
+                        pictureBox[1 + PosY - 1, 6 + X].BackColor = Color.Purple;
+                        OldY = PosY - 1;
+                        break;
+                    case 7:
+                        pictureBox[0 + PosY - 1, 5 + X].BackColor = Color.Red;
+                        pictureBox[0 + PosY - 1, 4 + X].BackColor = Color.Red;
+                        pictureBox[1 + PosY - 1, 5 + X].BackColor = Color.Red;
+                        pictureBox[1 + PosY - 1, 6 + X].BackColor = Color.Red;
+                        OldY = PosY - 1;
+                        break;
+                }
+                PosY++;
+            }
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            int xl = 0, yl = 0;
+            for(int y = 0;y < 20; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    pictureBox[y, x] = new PictureBox();
+                    pictureBox[y, x].Size = new Size(30, 30);
+                    pictureBox[y, x].Location = new Point(175+30*xl, 60+30*yl);
+                    this.Controls.Add(pictureBox[y, x]);
+                    xl++;
+                }
+                xl = 0; yl++;
+            }
+            xl = 0; yl = 0;
+            for (int y = 0; y < 4; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    pictureBoxNB[y, x] = new PictureBox();
+                    pictureBoxNB[y, x].Size = new Size(30, 30);
+                    pictureBoxNB[y, x].Location = new Point(35+30*xl,90+30*yl);
+                    this.Controls.Add(pictureBoxNB[y, x]);
+                    xl++;
+                }
+                xl = 0; yl++;
+            }
+        }
+    }
+}
+
+/*
+ long:
+  pictureBoxNB[0, 1].BackColor = Color.LightBlue;
+  pictureBoxNB[1, 1].BackColor = Color.LightBlue;
+  pictureBoxNB[2, 1].BackColor = Color.LightBlue;
+  pictureBoxNB[3, 1].BackColor = Color.LightBlue;
+ 
+ l to the right:
+  pictureBoxNB[1, 0].BackColor = Color.Blue;
+  pictureBoxNB[2, 0].BackColor = Color.Blue;
+  pictureBoxNB[2, 1].BackColor = Color.Blue;
+  pictureBoxNB[2, 2].BackColor = Color.Blue;
+
+ l to the left:
+  pictureBoxNB[1, 2].BackColor = Color.Orange;
+  pictureBoxNB[2, 0].BackColor = Color.Orange;
+  pictureBoxNB[2, 1].BackColor = Color.Orange;
+  pictureBoxNB[2, 2].BackColor = Color.Orange;
+
+ square:
+  pictureBoxNB[0, 0].BackColor = Color.Yellow;
+  pictureBoxNB[0, 1].BackColor = Color.Yellow;
+  pictureBoxNB[1, 0].BackColor = Color.Yellow;
+  pictureBoxNB[1, 1].BackColor = Color.Yellow;
+
+ z to the left:
+  pictureBoxNB[1, 1].BackColor = Color.Green;
+  pictureBoxNB[1, 2].BackColor = Color.Green;
+  pictureBoxNB[2, 0].BackColor = Color.Green;
+  pictureBoxNB[2, 1].BackColor = Color.Green;
+
+ z to the right:
+  pictureBoxNB[1, 0].BackColor = Color.Purple;
+  pictureBoxNB[1, 1].BackColor = Color.Purple;
+  pictureBoxNB[2, 1].BackColor = Color.Purple;
+  pictureBoxNB[2, 2].BackColor = Color.Purple;
+
+ t:
+  pictureBoxNB[1, 1].BackColor = Color.Red;
+  pictureBoxNB[2, 0].BackColor = Color.Red;
+  pictureBoxNB[2, 1].BackColor = Color.Red;
+  pictureBoxNB[2, 2].BackColor = Color.Red;
+ */
